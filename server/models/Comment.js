5bced25f12b78897e3c0bc7e7909eafc5c205536
Tevin Banton <tevinanthony95@gmail.com>
2021-03-09 12:11:20 -0500

    quick fix

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -13,12 +13,8 @@ const commentSchema = new Schema({
   likes: Number,
   replies: [],
   rootComment: String, // If this comment is a reply to another comment
-<<<<<<< HEAD
   marginDepth: Number, // This is used to determine how much margin is applied when displaying comment
   pageId: String, //The page the comment belongs to
-=======
-  depth: Number, // This is used to determine how much margin is applied when displaying comment
->>>>>>> 7ffa703e942896e8794537ea41ff58c36ccd6296
 });
 
 module.exports = Comment = mongoose.model('comment', commentSchema);
