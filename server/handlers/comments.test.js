ee4ddc498967740cd45ed6a357ebdae5cec51de2
Tevin Banton <tevinanthony95@gmail.com>
2021-03-11 12:57:29 -0500

    added all tests for the edit handler function

--- server/handlers/comments.test.js
+++ server/handlers/comments.test.js
@@ -1,22 +1,55 @@
 const { editHandler } = require('./comments');
 
-describe('Edit handler', () => {
+describe('Edit handler tests', () => {
   let mockUpdateDoc = jest.fn(() => 'doc updated');
-  let mockResponse = {
-    statusCode: null,
-    json: jest.fn((obj) => obj),
-    status: jest.fn((statusCode) => {
-      this.statusCode = statusCode;
-      return this;
-    }),
-  };
+  let mockResponse;
+  let errorMessage = 'error';
 
   //reset mockResponse before each test
+  beforeEach(() => {
+    mockResponse = {
+      statusCode: null,
+      json: jest.fn((obj) => obj),
+      status: jest.fn((statusCode) => {
+        mockResponse.statusCode = statusCode;
+        return mockResponse;
+      }),
+    };
+  });
 
-  it('ensures that the update doc function is called once', () => {
-    editHandler(mockResponse, mockUpdateDoc, 'error');
+  it('should call updateDoc() only once', async () => {
+    await editHandler(mockResponse, mockUpdateDoc, errorMessage);
 
-    //expects mockUpdateDoc to be called once
     expect(mockUpdateDoc.mock.calls.length).toBe(1);
+  });
+
+  it('should call response.json() only once for happy path', async () => {
+    await editHandler(mockResponse, mockUpdateDoc, errorMessage);
+
+    expect(mockResponse.json.mock.calls.length).toBe(1);
+  });
+
+  it('should call response.json() with the value from updateDoc()', async () => {
+    await editHandler(mockResponse, mockUpdateDoc, errorMessage);
+
+    expect(mockResponse.json).toBeCalledWith('doc updated');
+  });
+
+  it('should call response.status() only once for failing path', async () => {
+    await editHandler(mockResponse, null, errorMessage);
+
+    expect(mockResponse.status.mock.calls.length).toBe(1);
+  });
+
+  it('should have a status code of 400 for the failing path', async () => {
+    await editHandler(mockResponse, null, errorMessage);
+
+    expect(mockResponse.statusCode).toBe(400);
+  });
+
+  it('should call response.json() with the error message given for the failing path', async () => {
+    await editHandler(mockResponse, null, errorMessage);
+
+    expect(mockResponse.json).toBeCalledWith({ msg: errorMessage });
   });
 });
