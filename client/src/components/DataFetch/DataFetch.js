5fe796ab99658fb213470e8469c7c1381c26569a
Tevin Banton <67722040+TevinAB@users.noreply.github.com>
2021-03-17 17:10:29 -0500

    added data fetch component (#10)

---
+++ client/src/components/DataFetch/DataFetch.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+function DataFetch({ path, render }) {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(path)
+      .then((response) => setData(response.data))
+      .catch((error) => setError(true));
+  }, [path]);
+
+  if (error)
+    return (
+      <div>
+        <h2>An Error has occured.</h2>
+      </div>
+    );
+
+  return render(data);
+}
+
+DataFetch.propTypes = {
+  path: PropTypes.string.isRequired,
+  render: PropTypes.func.isRequired,
+};
+
+export default DataFetch;
