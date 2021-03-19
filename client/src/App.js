b291ac355218d2d4c1e976dfa8530d6d87794530
Tevin Banton <tevinanthony95@gmail.com>
2021-03-19 08:08:37 -0500

    small clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,9 @@
 import Container from './components/Container';
-import IconButton from './components/IconButton';
 
 function App() {
   return (
     <Container>
-      <div>
-        Hello
-        <IconButton
-          iconClass='fas fa-users'
-          tooltip='profile'
-          ariaLabel='profile'
-          buttonType='submit'
-          onClick={() => console.log('text')}
-          iconStyle={{ color: '#999999' }}
-        />
-      </div>
+      <div>Hello</div>
     </Container>
   );
 }
