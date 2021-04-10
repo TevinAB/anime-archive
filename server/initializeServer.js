0137d7ba6cfa9f0d8345d140e5ef0accee1ecb49
Tevin Banton <tevinanthony95@gmail.com>
2021-04-09 21:46:14 -0500

    serving static hmtl to fix issue

--- server/initializeServer.js
+++ server/initializeServer.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const api = require('./routes');
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+//used to serve the index.html in order to fix error that stems from client side routing and heroku in prod
+if (process.env.NODE_ENV === 'production') {
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
+
 app.use('/api', api);
 
 module.exports = app;
