065c1544f4a78e2a5f5cfbaf3eb437c8eee7828d
Tevin Banton <tevinanthony95@gmail.com>
2021-04-09 23:45:10 -0500

    .gitignore fix

--- server/initializeServer.js
+++ server/initializeServer.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const api = require('./routes');
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+//used to serve the index.html in order to fix error that stems from client side routing and heroku in prod
+if (process.env.NODE_ENV === 'production') {
+  //regex to catch the homepage '/'
+  app.get('^/$', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
+
 app.use('/api', api);
 
 module.exports = app;
