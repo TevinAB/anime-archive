be8585fd782d1ff5b048787404e25d997408aa4b
Tevin Banton <tevinanthony95@gmail.com>
2021-06-03 20:30:44 -0500

    fixed the bug that wasn't a bug

--- server/initializeServer.js
+++ server/initializeServer.js
@@ -7,14 +7,9 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-//used to serve the index.html in order to fix error that stems from client side routing and heroku in prod
-if (process.env.NODE_ENV === 'production') {
-  //regex to catch the homepage '/'
-  app.get('^/$', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
-}
-
 app.use('/api', api);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 module.exports = app;
